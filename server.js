82c0d874e4ca89b86ce179dc954f50951a90e46a
Dexter Sampson <dexter1.sampson@gmail.com>
2020-05-03 13:41:50 -0400

    serverjs updated

---
+++ server.js
@@ -0,0 +1,24 @@
+
+// DEPENDENCIES
+var express = require("express");
+
+// EXPRESS CONFIGURATION
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ROUTES
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
