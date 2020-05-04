0056f008c6fa68c582877e10cab46f759f00cde3
Dexter Sampson <dexter1.sampson@gmail.com>
2020-05-04 14:33:08 -0400

    update api

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -7,5 +7,11 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
       res.json(friendData);
     });
-  
+    
+    app.post("/api/friends", function(req, res) {
+        fiendData.push(req.body);
+        res.json(true);
+      
+      
+    });
   };
